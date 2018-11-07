3949e826d17d9a3ea54fcfc0d12bf2e25137a2b4
BouhouchJulien <jujubouhouch@live.fr>
2018-11-07 15:03:34 +0100

    suppression attribut deplacable / methode getDeplacable

--- src/modele/Rectangle.java
+++ src/modele/Rectangle.java
@@ -7,9 +7,9 @@ public class Rectangle extends Forme{
 	private Point premierPoint;
 	private Point deuxiemePoint;
 	
-	public Rectangle(boolean deplacable, Point premierPoint, Point deuxiemePoint) {
+	public Rectangle(Point premierPoint, Point deuxiemePoint) {
 		
-		this.deplacable = deplacable;
+		
 		this.premierPoint = premierPoint;
 		this.deuxiemePoint = deuxiemePoint;
 		
@@ -17,7 +17,7 @@ public class Rectangle extends Forme{
 	
 	@Override
 	public boolean deplacer(Point pointDepart, Point pointArrive) {
-		if ( this.deplacable && true ) { // TO DO - Remplacer true par la vérification du déplacement
+		if (true ) { // TO DO - Remplacer true par la vérification du déplacement
 			
 			Point distance = new Point ((int)(pointArrive.getX() - pointDepart.getX()), (int) (pointArrive.getY() - pointDepart.getY()));
 			this.premierPoint = new Point((int)(this.premierPoint.getX() + distance.getX()),(int)(this.premierPoint.getY()+ distance.getY()));
@@ -27,8 +27,6 @@ public class Rectangle extends Forme{
 		return false;
 	}
 
-	public boolean getDeplacable() {
-		return this.deplacable;
-	}
+	
 
 }
