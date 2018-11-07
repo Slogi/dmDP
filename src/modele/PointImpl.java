283429a1740bb752dd96444a3b610eeaf25668d0
BouhouchJulien <jujubouhouch@live.fr>
2018-11-07 15:03:06 +0100

    suppression attribut deplacable / methode getDeplacable

--- src/modele/PointImpl.java
+++ src/modele/PointImpl.java
@@ -6,12 +6,11 @@ public class PointImpl extends Forme {
 
 	private Point point;
 	
-	public PointImpl(boolean deplacable, Point point) {
-		this.deplacable = deplacable;
+	public PointImpl(Point point) {
 		this.point = point;
 	}
 	public boolean deplacer(Point pointDepart, Point pointArrive) {
-		if ( this.deplacable && true ) { // TO DO - Remplacer true par la vérification du déplacement
+		if ( true ) { // TO DO - Remplacer true par la vérification du déplacement
 			
 			Point distance = new Point ((int)(pointArrive.getX() - pointDepart.getX()), (int) (pointArrive.getY() - pointDepart.getY()));
 			this.point = new Point((int)(this.point.getX() + distance.getX()),(int)(this.point.getY()+ distance.getY()));
@@ -20,10 +19,6 @@ public class PointImpl extends Forme {
 		return false;
 	}
 
-	@Override
-	public boolean getDeplacable() {
-		// TODO Auto-generated method stub
-		return this.deplacable;
-	}
-
 }
+
+
