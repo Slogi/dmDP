ec4f0505d4a0eed14967668daabda8ac1dbf27dc
BouhouchJulien <jujubouhouch@live.fr>
2018-11-07 15:02:15 +0100

    suppression attribut deplacable / methode getDeplacable

--- src/modele/Forme.java
+++ src/modele/Forme.java
@@ -6,9 +6,9 @@ import util.observer.AbstractModeleEcoutable;
 
 public abstract class Forme extends AbstractModeleEcoutable {
 	
-	protected boolean deplacable;
+	//protected boolean deplacable;
 	
 	public abstract boolean deplacer(Point pointDepart, Point pointArrive);
 	
-	public abstract boolean getDeplacable();
+	//public abstract boolean getDeplacable();
 }
